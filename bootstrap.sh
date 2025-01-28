e40b827251ac6276c43f32a8b934cda5f273b55b
88victory88 <victory.otrok@gmail.com>
2025-01-28 14:05:42 +0800

    Solution2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+kind create cluster --config cluster.yml
+helm dependency update helm-chart/todoapp
+helm install todoapp helm-chart/todoapp
+
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
